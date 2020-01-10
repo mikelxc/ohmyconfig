2c50d352b5f8de0e29ef1e96c08e61e30ca16d2f
Mike Liu <mike000708@me.com>
2020-01-10 16:51:45 +0800

    Update docker-ubuntu.sh

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -17,3 +17,6 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker 
 docker run hello-world
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
