71432efaf91e377bdef27b8fcc13607f8a2a2f4e
Mike Liu <mike000708@me.com>
2020-01-11 13:59:33 +0800

    add mosh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,6 +1,10 @@
+#mosh true color
+sudo add-apt-repository ppa:keithw/mosh-dev
+#get docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install \
+    mosh \
     apt-transport-https \
     ca-certificates \
     curl \
